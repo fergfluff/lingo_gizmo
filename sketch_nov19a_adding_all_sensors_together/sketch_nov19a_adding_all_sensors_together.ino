ca33686172197f120f13fe4706053c3ea83b4246
Elizabeth Ferguson <elizabeth.j.ferguson@gmail.com>
2017-11-19 16:36:14 -0500

    Clean serial monitor data, need to turn into an array

--- sketch_nov19a_adding_all_sensors_together/sketch_nov19a_adding_all_sensors_together.ino
+++ sketch_nov19a_adding_all_sensors_together/sketch_nov19a_adding_all_sensors_together.ino
@@ -37,18 +37,7 @@ void setup() {
 void loop() {
   //if (Serial.available() > 0) {
 
-  //Read mouth0Sensor to see if the mouth is pressed
-  int mouthPressed = digitalRead(mouth0Sensor);
-  //Map mouthPressed to off or on / 0 or 1
-  //? Can I reassign different values ot the same variable? or needs to be different?
-  if (mouthPressed == LOW) {
-    //mouthNotPressed = 0;
-    //mouthPressed = 0;
-    Serial.println("Mouth not pressed");
-  }  else if (mouthPressed == HIGH) {
-    //mouthPressed = 1;
-    Serial.println("Mouth PRESSED");
-  }
+
 
   //Read the accelerometer's values
   float ax, ay, az;   //scaled accelerometer values
@@ -58,89 +47,115 @@ void loop() {
   if (ax >= 0.50) {
     ax = 2;
     //mouthOpen = ax;
-    Serial.print("Mouth open:");
+    Serial.print("mouth open:");
     Serial.print("\t");
     Serial.println(ax);
   } else if (ax < 0.49 && ax > -0.510) {
     ax = 1;
-    Serial.print("Mouth halfway:");
+    Serial.print("mouth halfway:");
     Serial.print("\t");
     Serial.println(ax);
   } else if (ax < -0.50) {
     ax = 0;
-    Serial.print("Mouth closed:");
+    Serial.print("mouth closed:");
     Serial.print("\t");
     Serial.println(ax);
   }
 
 
   //COMBINE SENSORS TO CREATE USEFUL VALUES
-
-
   //MOUTH SENSORS
+  //Read mouth0Sensor to see if the mouth is pressed
+  int mouthPressed = digitalRead(mouth0Sensor);
+  if (mouthPressed == LOW) {
+    mouthPressed = 0;
+    Serial.print("no mouth pressed:");
+    Serial.print("\t");
+    Serial.println(mouthPressed);
+  }  else if (mouthPressed == HIGH) {
+    mouthPressed = 1;
+    Serial.print("yes mouth pressed");
+    Serial.print("\t");
+    Serial.println(mouthPressed);
+  }
   //Mouth is closed completely, consonants play (such as mah, pah, mah)
   if (mouthPressed == 0 && ax == 0) {
     //How do I store this into a new variable? Such as ClosedConsonants.
     //closedConsonants = 1;
-    Serial.println("consonants");
+    Serial.println("yes consonants");
     //Mouth is NOT pressed at all, no sounds play.
-  } else if (mouthPressed == 0) {
-    //noSound = 1;
-    Serial.println("no sound");
     //Mouth is pressed and mouth is open, low vowels play
   } else if (mouthPressed == 1 && ax == 2) {
     //lowVowels = 0;
-    Serial.println("low vowels list");
+    Serial.println("yes low vowels list");
     //Mouth is pressed and mouth is halfway open, high vowels play
   } else if (mouthPressed == 1 && ax == 1) {
     //highVowels = 0;
-    Serial.println("high vowels list");
+    Serial.println("yes high vowels list");
   }
 
   //TEETH SENSOR
-  int teethPressed = digitalRead(teeth3Sensor);
-  if (teethPressed == LOW) {
-    //teethPressed = 0;
-    Serial.println("no teeth sound");
-  } else if (teethPressed == HIGH) {
-    //teethPressed = 1;
-    Serial.println("teeth sound");
+  int teethPressed = analogRead(teeth3Sensor);
+  if (teethPressed < 799) {
+    teethPressed = 0;
+    Serial.print("no teeth sound:");
+    Serial.print("\t");
+    Serial.println(teethPressed);
+  } else if (teethPressed > 800) {
+    teethPressed = 1;
+    Serial.print("yes teeth sound:");
+    Serial.print("\t");
+    Serial.println(teethPressed);
   }
 
   //TONGUE SENSORS
-  int tipTonguePressed = digitalRead(tongue4Sensor);
-  if (tipTonguePressed == LOW) {
-    //tipTonguePressed = 0;
-    Serial.println("no tip of tongue sounds");
-  } else if (tipTonguePressed == HIGH) {
-    //tipTonguePressed = 1;
-    Serial.println("tip of tongue sounds");
+  int tipTonguePressed = analogRead(tongue4Sensor);
+  if (tipTonguePressed < 799) {
+    tipTonguePressed = 0;
+    Serial.print("no tip of tongue sounds:");
+    Serial.print("\t");
+    Serial.println(tipTonguePressed);
+  } else if (tipTonguePressed > 800) {
+    tipTonguePressed = 1;
+    Serial.print("yes tip of tongue sounds:");
+    Serial.print("\t");
+    Serial.println(tipTonguePressed);
   }
 
-  int middleTonguePressed = digitalRead(tongue5Sensor);
-  if (middleTonguePressed == LOW) {
-    //middleTonguePressed = 0;
-    Serial.println("no middle of tongue sounds");
-  } else if (middleTonguePressed == HIGH) {
-    //middleTonguePressed = 1;
-    Serial.println("middle of tongue sounds");
+  int middleTonguePressed = analogRead(tongue5Sensor);
+  if (middleTonguePressed < 799) {
+    middleTonguePressed = 0;
+    Serial.print("no middle of tongue sounds:");
+    Serial.print("\t");
+    Serial.println(middleTonguePressed);
+  } else if (middleTonguePressed > 800) {
+    middleTonguePressed = 1;
+    Serial.print("yes middle of tongue sounds:");
+    Serial.print("\t");
+    Serial.println(middleTonguePressed);
   }
 
-  int backTonguePressed = digitalRead(tongue6Sensor);
-  if (backTonguePressed == LOW) {
-    //backTonguePressed = 0;
-    Serial.println("no back of tongue sounds");
-  } else if (backTonguePressed == HIGH) {
-    //backTonguePressed = 1;
-    Serial.println("back of tongue sounds");
-    Serial.print("");
-    Serial.print("");
+  int backTonguePressed = analogRead(tongue6Sensor);
+  if (backTonguePressed < 799) {
+    backTonguePressed = 0;
+    Serial.print("no back of tongue sounds:");
+    Serial.print("\t");
+    Serial.println(backTonguePressed);
+  } else if (backTonguePressed > 800) {
+    backTonguePressed = 1;
+    Serial.print("yes back of tongue sounds:");
+    Serial.print("\t");
+    Serial.println(backTonguePressed);
   }
+
+  Serial.println("-");
+  Serial.println("-");
 
   //HOW DO I PRINT DATA IN A CLEAN LIST FOR SERIAL COMMUNICATION?
+  //Serial.println[mouthPressed + teethPressed + tipTonguePressed + middleTonguePressed + backTonguePressed];
 
-  //Serial.println(mouthPressed, closedConsonants, noSound, lowVowels, highVowels, 
-  //teethPressed, tipTonguePressed, middleTonguePressed, backTonguePressed);
+  //closedConsonants, noSound, lowVowels, highVowels,
+
   //  mouthPressed;
   //  closedConsonants;
   //  noSound;
@@ -151,5 +166,5 @@ void loop() {
   //  middleTonguePressed;
   //  backTonguePressed;
 
-delay(1000);
+  delay(1000);
 }
